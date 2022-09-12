f528781a697eaf33a560ca6b4400d861b2b4d857
Andrew Heyer <andrewbheyer@gmail.com>
2022-09-12 02:48:27 -0700

    Early revision of logo, intro summary and svg's icons on home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,20 +6,19 @@ const Hero = () => {
             <div className="container col-xxl-8 px-4 py-5">
                 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                     <div className="col-10 col-sm-8 col-lg-6">
-                        <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid"
+                        <img src="andrewheyer_observatory_pose1.png" className="d-block mx-lg-auto img-fluid"
                              alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
                     </div>
                     <div className="col-lg-6">
-                        <h1 className="display-5 fw-bold lh-1 mb-3">Andrew Heyer</h1>
-                        <p className="lead">Quickly design and customize responsive mobile-first sites with
-                            Bootstrap, the world’s
-                            most popular front-end open source toolkit, featuring Sass variables and mixins,
-                            responsive grid system,
-                            extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
-                        </div>
+                        <h1 className="display-5 fw-bold lh-1 mb-3">Hello, World</h1>
+                        <p className={"mt-2"}>I’m excited to present v1 of my portfolio website. <code>You will.find(
+                            3_projects ) foreach</code> one is focused on concepts and practical
+                            implementations of React, Django, and AWS. I created these projects to level up my
+                            skill-set and confidence within the full stack software domain.
+                        </p>
+                        <p> Thanks for your time.
+                        </p>
+                        <p> - Andrew Heyer</p>
                     </div>
                 </div>
             </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,30 +4,25 @@ const Nav = () => {
     return (
         <div>
             <main>
-
-                <svg xmlns="http://www.w3.org/2000/svg" display={"none"}>
-                    <symbol id="bootstrap" viewBox="0 0 118 94">
-                        <title>Bootstrap</title>
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-                    </symbol>
-                </svg>
-
                 <div className="container">
                     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                         <a href="/"
                            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                            <svg className="bi me-2" width="40" height="32">
-                                <use href="#bootstrap"/>
+                            <span className="fs-4">full</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="26.000000pt" height="28.000000pt" viewBox="0 0 262.000000 286.000000" preserveAspectRatio="xMidYMid meet">
+                                <g transform="translate(0.000000,286.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+                                    <path d="M830 1914 c-256 -151 -477 -289 -492 -305 -33 -34 -34 -49 -10 -77 27 -32 952 -552 982 -552 26 0 939 515 971 548 30 30 32 58 7 85 -27 30 -954 577 -978 577 -8 0 -224 -124 -480 -276z m905 -114 c198 -116 363 -214 367 -218 7 -7 -778 -462 -796 -462 -20 0 -795 445 -788 452 4 4 185 113 402 242 l395 234 30 -18 c17 -10 192 -113 390 -230z"/>
+                                    <path d="M1770 1009 c-250 -144 -459 -262 -465 -262 -5 1 -212 116 -460 257 -247 141 -460 256 -472 256 -24 0 -63 -43 -63 -70 0 -9 8 -25 19 -34 39 -35 957 -549 981 -549 32 0 935 515 973 554 15 16 27 38 27 48 0 22 -44 61 -69 61 -9 0 -221 -118 -471 -261z"/>
+                                    <path d="M1765 635 l-460 -264 -40 23 c-312 182 -878 496 -893 496 -23 0 -62 -44 -62 -70 0 -9 10 -26 23 -37 40 -37 949 -546 976 -547 17 0 196 97 503 273 504 290 516 298 489 350 -12 22 -42 41 -63 41 -7 -1 -220 -120 -473 -265z"/>
+                                </g>
                             </svg>
-                            <span className="fs-4">Simple header</span>
+                            <span className="fs-4">heyer.me</span>
                         </a>
                         <ul className="nav nav-pills">
                             <li className="nav-item"><a href="/#" className="nav-link active"
-                                                        aria-current="page">Home</a></li>
-                            <li className="nav-item"><a href="/#" className="nav-link">Features</a></li>
-                            <li className="nav-item"><a href="/#" className="nav-link">Pricing</a></li>
-                            <li className="nav-item"><a href="/#" className="nav-link">FAQs</a></li>
-                            <li className="nav-item"><a href="/#" className="nav-link">About</a></li>
+                                                        aria-current="page">Projects</a></li>
+                            <li className="nav-item"><a href="/#" className="nav-link">Resume</a></li>
+                            <li className="nav-item"><a href="/#" className="nav-link">Gatitas</a></li>
                         </ul>
                     </header>
                 </div>
